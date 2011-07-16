6c6e06d1541f4ab91312eba302a2170ec1c12751
ajlopez <webmaster@ajlopez.com>
2011-07-16 20:13:02 -0300

    Alarm, second test

--- AjVars.Tests/AlarmTests.cs
+++ AjVars.Tests/AlarmTests.cs
@@ -27,5 +27,20 @@
             this.integerVariable.Value = 30;
             Assert.AreEqual(1, count);
         }
+
+        [TestMethod]
+        public void RaiseNoAlarm()
+        {
+            int count = 0;
+            this.integerVariable.Value = 20;
+            Alarm alarm = new Alarm<int>(this.integerVariable, (oldvalue, newvalue) => newvalue == 30);
+            alarm.NewAlarm += (oldvalue, newvalue) => count++;
+
+            for (int k = 1; k <= 100; k++)
+                if (k != 30)
+                    this.integerVariable.Value = k;
+
+            Assert.AreEqual(0, count);
+        }
     }
 }
