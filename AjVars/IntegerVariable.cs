1f3e0be2e351a7d009349264dfec8504312a3be5
ajlopez <webmaster@ajlopez.com>
2011-07-16 12:04:39 -0300

    First test: compile OK, but red

---
+++ AjVars/IntegerVariable.cs
@@ -0,0 +1,22 @@
+﻿namespace AjVars
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class IntegerVariable : Variable
+    {
+        public override object Value
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ AjVars/Variable.cs
@@ -0,0 +1,12 @@
+﻿namespace AjVars
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public abstract class Variable
+    {
+        public abstract object Value { get; set; }
+    }
+}
