20de1a0202626c6ac6134ddd819e1f1bf0852c5b
ajlopez <webmaster@ajlopez.com>
2011-07-16 12:47:31 -0300

    ByteMemory, first test in red

---
+++ AjVars.Tests/ByteMemoryTests.cs
@@ -0,0 +1,23 @@
+﻿namespace AjVars.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ByteMemoryTests
+    {
+        [TestMethod]
+        public void GetFirstBytesFromMemory()
+        {
+            ByteMemory bytememory = new ByteMemory();
+            byte[] bytes = bytememory.GetBytes(0, 10);
+
+            Assert.IsNotNull(bytes);
+            Assert.AreEqual(10, bytes.Length);
+            Assert.IsTrue(bytes.All(b => b == 0));
+        }
+    }
+}
---
+++ AjVars/ByteMemory.cs
@@ -0,0 +1,15 @@
+﻿namespace AjVars
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class ByteMemory
+    {
+        public byte[] GetBytes(int p, int p_2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
