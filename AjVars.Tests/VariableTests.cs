0eced0867e8402fb8108d907c7fa9caf554af02b
ajlopez <webmaster@ajlopez.com>
2011-07-16 12:02:09 -0300

    First test: compile fail!

---
+++ AjVars.Tests/VariableTests.cs
@@ -0,0 +1,19 @@
+﻿namespace AjVars.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class VariableTests
+    {
+        [TestMethod]
+        public void CreateIntegerVariableAndGetValue()
+        {
+            Variable variable = new IntegerVariable();
+            Assert.AreEqual(0, variable.Value);
+        }
+    }
+}
