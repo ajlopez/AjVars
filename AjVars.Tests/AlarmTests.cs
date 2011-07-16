193d4b37764da0780abbc1e0c6b3b983111c059d
ajlopez <webmaster@ajlopez.com>
2011-07-16 20:11:24 -0300

    Alarm with first test

---
+++ AjVars.Tests/AlarmTests.cs
@@ -0,0 +1,31 @@
+﻿namespace AjVars.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AlarmTests
+    {
+        private Variable<int> integerVariable;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.integerVariable = new IntegerVariable(0, new ByteMemory());
+        }
+
+        [TestMethod]
+        public void RaiseAlarm()
+        {
+            int count = 0;
+            this.integerVariable.Value = 20;
+            Alarm alarm = new Alarm<int>(this.integerVariable, (oldvalue, newvalue) => newvalue == 30);
+            alarm.NewAlarm += (oldvalue, newvalue) => count++;
+            this.integerVariable.Value = 30;
+            Assert.AreEqual(1, count);
+        }
+    }
+}
---
+++ AjVars/Alarm.cs
@@ -0,0 +1,40 @@
+﻿namespace AjVars
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public abstract class Alarm
+    {
+        public delegate void AlarmHandler(object oldvalue, object newvalue);
+
+        public event AlarmHandler NewAlarm;
+
+        public void RaiseNewAlarm(object oldvalue, object newvalue)
+        {
+            if (this.NewAlarm != null)
+                this.NewAlarm(oldvalue, newvalue);
+        }
+    }
+
+    public class Alarm<T> : Alarm
+    {
+        private Variable<T> variable;
+        private Func<T, T, bool> condition;
+
+        public Alarm(Variable<T> variable, Func<T, T, bool> condition)
+        {
+            this.variable = variable;
+            this.condition = condition;
+
+            this.variable.NewValue += this.NewValue;
+        }
+
+        private void NewValue(object oldvalue, object newvalue)
+        {
+            if (this.condition((T) oldvalue, (T) newvalue))
+                this.RaiseNewAlarm(oldvalue, newvalue);
+        }
+    }
+}
