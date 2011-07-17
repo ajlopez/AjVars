7bcf4d7587d0360f8357166b8491ec4ed559c68b
ajlopez <webmaster@ajlopez.com>
2011-07-17 13:37:51 -0300

    ShortTypeValue with first tests Additional tests for IntegerTypeValue

--- AjVars.Tests/TypeValueTests.cs
+++ AjVars.Tests/TypeValueTests.cs
@@ -18,5 +18,47 @@
             Assert.AreEqual(1000, type.ParseString("1000"));
             Assert.AreEqual(-2000, type.ParseString("-2000"));
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void RaiseIfTryParseFloatAsInteger()
+        {
+            TypeValue type = new IntegerTypeValue();
+            type.ParseString("1.2");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void RaiseIfTryParseTextAsInteger()
+        {
+            TypeValue type = new IntegerTypeValue();
+            type.ParseString("foo");
+        }
+
+        [TestMethod]
+        public void ShortTypeValueParseStrings()
+        {
+            TypeValue type = new ShortTypeValue();
+
+            Assert.AreEqual((short) 1, type.ParseString("1"));
+            Assert.AreEqual((short) 1000, type.ParseString("1000"));
+            Assert.AreEqual((short) -2000, type.ParseString("-2000"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void RaiseIfTryParseFloatAsShort()
+        {
+            TypeValue type = new ShortTypeValue();
+            type.ParseString("1.2");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void RaiseIfTryParseTextAsShort()
+        {
+            TypeValue type = new ShortTypeValue();
+            type.ParseString("foo");
+        }
     }
 }
